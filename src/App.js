80df1fc9ab9310f3b9615804c58f6557570f08d2
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2022-02-04 17:57:41 -0500

    removing un used vars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Routes, Route, Link} from 'react-router-dom';
-import {Layout, Typography, Space} from 'antd';
+import {Routes, Route} from 'react-router-dom';
+import {Layout, Typography} from 'antd';
 import {Navbar, Exchanges,Cryptocurrencies,CryptoDetails,Homepage,News} from './components'
 import './App.css';
 const App = () => {
